109a5031595defcabe2f956f4bda5db5d04c0b8d
giperbaba <aue64@list.ru>
2024-05-09 23:15:31 +0700

    depressed✓ add margin and padding for fragments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.activity)
+    implementation(libs.androidx.databinding.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/photoeditor/ui/unsharpMask/UnsharpFragment.kt
+++ app/src/main/java/com/example/photoeditor/ui/unsharpMask/UnsharpFragment.kt
@@ -5,16 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.photoeditor.R
+import com.example.photoeditor.databinding.FragmentUnsharpBinding
+
 
 class UnsharpFragment : Fragment() {
+
+    private lateinit var binding: FragmentUnsharpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_unsharp, container, false)
+        binding = FragmentUnsharpBinding.inflate(inflater, container, false)
+        return binding.root
     }
-
 }
