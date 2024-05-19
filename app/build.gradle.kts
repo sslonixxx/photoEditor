4e4cbfb10f4abe2ad7930c5591134f7b9edc4f49
giperbaba <aue64@list.ru>
2024-05-19 14:42:01 +0700

    New Feature - added save photo to user gallery ;) 0_x x_x 0_0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.databinding.runtime)
+    implementation(libs.play.services.cast.framework)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
