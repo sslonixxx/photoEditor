1a9c982c38ff6b31c773fa084403fa278eeb1aa4
gerget <gergetsofya2005@gmail.com>
2024-05-20 13:59:49 +0700

    Refactor RetouchFragment after merge request

--- app/src/main/java/com/example/photoeditor/ui/retouch/RetouchFragment.kt
+++ app/src/main/java/com/example/photoeditor/ui/retouch/RetouchFragment.kt
@@ -1,60 +1,202 @@
 package com.example.photoeditor.ui.retouch
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.photoeditor.databinding.FragmentRetouchBinding
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
+import android.view.MotionEvent
+import android.widget.ImageView
+import android.widget.Toast
 import com.example.photoeditor.R
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.sqrt
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RetouchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RetouchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private var _binding: FragmentRetouchBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var imageView: ImageView
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_retouch, container, false)
+    ): View {
+        _binding = FragmentRetouchBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment retouchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RetouchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        var rateOfRetouching: Float = 0f
+        var sizeOfBrush: Float = 0f
+
+        binding.rateOfRetouchingSlider.addOnChangeListener { _, value, _ ->
+            rateOfRetouching = value
+        }
+        binding.sizeOfBrushSlider.addOnChangeListener { _, value, _ ->
+            sizeOfBrush = value
+        }
+        var lastX = 0
+        var lastY = 0
+        imageView = activity?.findViewById(R.id.imageView)!!
+        imageView.setOnTouchListener { _, event ->
+            try {
+                val imageMatrix = imageView.imageMatrix
+                val drawable = imageView.drawable
+                val rect = drawable.bounds
+                val transformedPoint = FloatArray(2)
+
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        transformedPoint[0] = event.x
+                        transformedPoint[1] = event.y
+                        imageMatrix.invert(imageMatrix)
+                        imageMatrix.mapPoints(transformedPoint)
+                        lastX = transformedPoint[0].toInt()
+                        lastY = transformedPoint[1].toInt()
+                        true
+                    }
+                    MotionEvent.ACTION_MOVE -> {
+                        transformedPoint[0] = event.x
+                        transformedPoint[1] = event.y
+                        imageMatrix.invert(imageMatrix)
+                        imageMatrix.mapPoints(transformedPoint)
+                        val x = transformedPoint[0].toInt()
+                        val y = transformedPoint[1].toInt()
+                        val deltaX = x - lastX
+                        val deltaY = y - lastY
+
+                        val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+                        val newBitmap = calculatingAverageColors(bitmap, x, y,
+                            sizeOfBrush.toInt(), rateOfRetouching)
+                        imageView.setImageBitmap(newBitmap)
+
+                        lastX = x
+                        lastY = y
+                        true
+                    }
+                    else -> false
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                false
+            }
+        }
+
+    }
+
+
+    fun calculatingAverageColors(
+        bitmap: Bitmap,
+        x: Int,
+        y: Int,
+        sizeOfBrush: Int,
+        rateOfRetouching: Float
+    ): Bitmap {
+        val mutableBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+
+        val centerX = x
+        val centerY = y
+
+        val radiusSquared = sizeOfBrush * sizeOfBrush
+
+        // Суммируем цвета пикселей внутри круга
+        val colorsInsideCircle = mutableListOf<Int>()
+        for (i in max(0, centerX - sizeOfBrush) until min(bitmap.width, centerX + sizeOfBrush)) {
+            for (j in max(0, centerY - sizeOfBrush) until min(
+                bitmap.height,
+                centerY + sizeOfBrush
+            )) {
+                val distanceSquared = (i - centerX) * (i - centerX) + (j - centerY) * (j - centerY)
+                if (distanceSquared <= radiusSquared) {
+                    colorsInsideCircle.add(bitmap.getPixel(i, j))
                 }
             }
+        }
+
+        // Вычисляем средний цвет
+        var sumRed = 0
+        var sumGreen = 0
+        var sumBlue = 0
+        for (color in colorsInsideCircle) {
+            sumRed += Color.red(color)
+            sumGreen += Color.green(color)
+            sumBlue += Color.blue(color)
+        }
+        val avgRed = sumRed / colorsInsideCircle.size
+        val avgGreen = sumGreen / colorsInsideCircle.size
+        val avgBlue = sumBlue / colorsInsideCircle.size
+
+        // Заполняем пиксели внутри круга цветом, близким к изначальному цвету пикселя, с учетом rateOfRetouching
+        for (i in max(0, centerX - sizeOfBrush) until min(bitmap.width, centerX + sizeOfBrush)) {
+            for (j in max(0, centerY - sizeOfBrush) until min(
+                bitmap.height,
+                centerY + sizeOfBrush
+            )) {
+                val distanceSquared = (i - centerX) * (i - centerX) + (j - centerY) * (j - centerY)
+                if (distanceSquared <= radiusSquared) {
+                    val distanceFactor = sqrt(distanceSquared.toFloat()) / sizeOfBrush
+                    val red = (avgRed + (Color.red(
+                        bitmap.getPixel(
+                            i,
+                            j
+                        )
+                    ) - avgRed) * distanceFactor * rateOfRetouching).toInt()
+                    val green = (avgGreen + (Color.green(
+                        bitmap.getPixel(
+                            i,
+                            j
+                        )
+                    ) - avgGreen) * distanceFactor * rateOfRetouching).toInt()
+                    val blue = (avgBlue + (Color.blue(
+                        bitmap.getPixel(
+                            i,
+                            j
+                        )
+                    ) - avgBlue) * distanceFactor * rateOfRetouching).toInt()
+                    mutableBitmap.setPixel(i, j, Color.rgb(red, green, blue))
+                }
+            }
+        }
+
+        return mutableBitmap
+    }
+
+    fun getBitmapFromImageView(imageView: ImageView): Bitmap {
+        // Получаем размеры ImageView
+        val width = imageView.width
+        val height = imageView.height
+
+        // Создаем Bitmap с такими же размерами, что и ImageView
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+
+        // Создаем Canvas, связанный с этим Bitmap
+        val canvas = Canvas(bitmap)
+
+        // Рисуем содержимое ImageView на Canvas
+        imageView.draw(canvas)
+
+        return bitmap
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
