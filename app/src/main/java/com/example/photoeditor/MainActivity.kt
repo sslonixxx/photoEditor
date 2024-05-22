28b85af3aa1e9576e8abbfbaa567f069f4daaef1
giperbaba <aue64@list.ru>
2024-05-22 11:47:15 +0700

    New filter - added unsharpMask 0_x x_x 0_0

--- app/src/main/java/com/example/photoeditor/MainActivity.kt
+++ app/src/main/java/com/example/photoeditor/MainActivity.kt
@@ -5,8 +5,14 @@ import android.Manifest.permission.READ_MEDIA_IMAGES
 import android.Manifest.permission.READ_MEDIA_VIDEO
 import android.Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED
 import android.app.Activity
+import android.content.ContentValues
+import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.view.View.MeasureSpec
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
@@ -16,14 +22,14 @@ import com.example.photoeditor.databinding.ActivityMainBinding
 import com.example.photoeditor.recyclerView.adapter.FilterAdapter
 import com.example.photoeditor.recyclerView.adapter.service.FilterGroupService
 import com.github.dhaval2404.imagepicker.ImagePicker
+import java.io.IOException
 
-
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var adapter: FilterAdapter
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,6 +65,19 @@ class MainActivity : AppCompatActivity() {
                     startForProfileImageResult.launch(intent)
                 }
         }
+
+        binding.saveButton.setOnClickListener {
+            imageView.isDrawingCacheEnabled = true
+            imageView.measure(MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED),
+                MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED))
+            imageView.layout(0, 0, imageView.measuredWidth, imageView.measuredHeight)
+            imageView.buildDrawingCache(true)
+            val newImage = Bitmap.createBitmap(imageView.drawingCache)
+            imageView.isDrawingCacheEnabled = false
+
+            saveImageToGallery(newImage)
+            //imageView.setImageBitmap(newImage)
+        }
     }
 
     private fun setupRecyclerView(navController: NavController) {
@@ -81,6 +100,40 @@ class MainActivity : AppCompatActivity() {
         binding.bestRecycler.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         binding.bestRecycler.adapter = adapter
+    }
+
+    private fun saveImageToGallery(bitmap: Bitmap) {
+        val resolver =
+            contentResolver // получает экземпляр ContentResolver для текущего контекста приложения
+        val fileName =
+            System.currentTimeMillis().toString() + ".png" // имя файла из даты времени и пнг
+
+        // хранилище мультимедийных файлов андроид медиастор
+        val contentValues = ContentValues().apply {// контейнер для хранения пар ключ-значение
+            put(MediaStore.MediaColumns.DISPLAY_NAME, fileName) // имя файла
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/png") // тип файла
+            // для андроида >= 10
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+            }
+        }
+        // вставляет новый файл в систему хранения файлов андроид
+        val imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+        try {
+            imageUri?.let { // если ури не нулл
+                resolver.openOutputStream(it)?.use { outputStream ->
+                    bitmap.compress(
+                        Bitmap.CompressFormat.PNG,
+                        100,
+                        outputStream
+                    ) // запись данных изображения в поток для записи данных в новый файл
+                }
+                Toast.makeText(this, "Image saved to gallery", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
     }
 
     private fun getPermission() {
