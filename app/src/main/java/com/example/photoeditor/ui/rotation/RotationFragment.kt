b8b086f52ced1f2f393f36e3dcff3081d61ce1c6
vikdays <vikdayss@gmail.com>
2024-05-24 17:09:15 +0700

    fix bags

--- app/src/main/java/com/example/photoeditor/ui/rotation/RotationFragment.kt
+++ app/src/main/java/com/example/photoeditor/ui/rotation/RotationFragment.kt
@@ -8,11 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import com.example.photoeditor.R
 import com.example.photoeditor.databinding.FragmentRotationBinding
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class RotationFragment : Fragment() {
 
@@ -30,21 +31,37 @@ class RotationFragment : Fragment() {
         val imageView = activity?.findViewById<ImageView>(R.id.imageView)!!
         val slider = binding.slider
         var previousSliderValue = slider.value // Сохраняем изначальное значение слайдера
+        val matrix = getPixelsFromImageView(imageView)
         slider.addOnChangeListener { _, value, _ ->
-            lifecycleScope.launch(Dispatchers.Default) {
-                val matrix = getPixelsFromImageView(imageView)
-                if (matrix != null) {
-                    val transposedMatrix = if (value > previousSliderValue) {
-                        transposePixelsMatrixby90(matrix)
-                    } else {
-                        transposePixelsMatrixby270(matrix)
+            if (matrix != null) {
+                CoroutineScope(Dispatchers.Main).launch {
+                    val transposedBitmap = withContext(Dispatchers.Default) {
+                        when (value) {
+                            90.toFloat() -> {
+                                val transposedMatrix = transposePixelsMatrixby90(matrix)
+                                createBitmapFromMatrix(transposedMatrix)
+                            }
+
+                            180.toFloat() -> {
+                                val transposedMatrix = transposePixelsMatrixby180(matrix)
+                                createBitmapFromMatrix(transposedMatrix)
+
+                            }
+
+                            270.toFloat() -> {
+                                val transposedMatrix = transposePixelsMatrixby270(matrix)
+                                createBitmapFromMatrix(transposedMatrix)
+                            }
+
+                            else -> {
+                                val transposedMatrix = matrix
+                                createBitmapFromMatrix(transposedMatrix)
+
+                            }
+                        }
                     }
-                    val transposedBitmap = createBitmapFromMatrix(transposedMatrix)
-                    imageView.post {
-                        imageView.setImageBitmap(transposedBitmap)
-                    }
+                    imageView.setImageBitmap(transposedBitmap)
                 }
-                previousSliderValue = value
             }
         }
         return root
@@ -61,16 +78,13 @@ class RotationFragment : Fragment() {
             val height = bitmap.height
             // Создаем матрицу для хранения пикселей
             val pixels = Array(height) { IntArray(width) }
-            // Проходимся по каждому пикселю изображения и сохраняем его в матрицу
             for (y in 0 until height) {
                 for (x in 0 until width) {
                     pixels[y][x] = bitmap.getPixel(x, y)
                 }
             }
-            // Возвращаем матрицу пикселей
             return pixels
         }
-        // Возвращаем null, если не удалось получить битмапу
         return null
     }
 
@@ -82,11 +96,12 @@ class RotationFragment : Fragment() {
         val transposedMatrix = Array(cols) { IntArray(rows) }
         for (i in 0 until rows) {
             for (j in 0 until cols) {
-                transposedMatrix[cols - 1 - j][i] = matrix[i][j]
+                transposedMatrix[j][rows - 1 - i] = matrix[i][j]
             }
         }
         return transposedMatrix
     }
+
 
     fun transposePixelsMatrixby90(matrix: Array<IntArray>): Array<IntArray> {
         val rows = matrix.size
@@ -101,14 +116,25 @@ class RotationFragment : Fragment() {
         return transposedMatrix
     }
 
+    private fun transposePixelsMatrixby180(matrix: Array<IntArray>): Array<IntArray> {
+        val rows = matrix.size
+        val cols = matrix[0].size
+        val rotatedMatrix = Array(rows) { IntArray(cols) }
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                rotatedMatrix[rows - 1 - i][cols - 1 - j] = matrix[i][j]
+            }
+        }
+        return rotatedMatrix
+    }
+
     fun createBitmapFromMatrix(matrix: Array<IntArray>): Bitmap {
         val rows = matrix.size
         val cols = matrix[0].size
         val bitmap = Bitmap.createBitmap(cols, rows, Bitmap.Config.ARGB_8888)
         for (i in 0 until rows) {
             for (j in 0 until cols) {
-                // Отражаем каждую строку
-                bitmap.setPixel(j, rows - 1 - i, matrix[i][j])
+                bitmap.setPixel(j, i, matrix[i][j]) // Пиксели не отражаются
             }
         }
         return bitmap
